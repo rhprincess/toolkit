45637c0d8c0fc6158ae063c9d19d836b51b64fdd
krinsofficial <45453071+krinsofficial@users.noreply.github.com>
2019-07-17 00:29:43 +0800

    Delete install.sh

--- install.sh
+++
@@ -1,4 +0,0 @@
-echo "\033[32m Starting... \033[0m"
-git clone https://github.com/krinsofficial/toolkit.git
-echo "\033[32m ---------------------------------------------- \033[0m"
-tree toolkit && echo "\033[32m Unzipping... \033[0m" && unzip toolkit/create.zip -d toolkit && tree toolkit && echo "\033[32m Installing... \033[0m" && chmod 777 toolkit/create && mv toolkit/create ../usr/bin/ && rm -rf toolkit && echo "\033[32m Success! \033[0m"
